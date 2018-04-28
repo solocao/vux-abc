4fc9095432dabd7af861a159feec7533bd8e5e0f
caowei <solocao@126.com>
2018-04-28 16:44:37 +0800

    better-scroll 简单demo

---
+++ src/components/BetterScroll.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <ul class="content">
+      <li>我就像那一只林中的小鸟</li>
+      <li>努力挣脱冲向蓝天怀抱</li>
+    </ul>
+  </div>
+</template>
+<script>
+import BScroll from 'better-scroll'
+export default {
+  mounted () {
+    this.$nextTick(() => {
+      this.scroll = new BScroll(this.$refs.wrapper, {})
+    })
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.wrapper {
+  position: relative;
+  height: 200px;
+  z-index: 1;
+  background: yellow;
+  overflow-y: hidden;
+}
+.content {
+  li {
+    background: red;
+    height: 300px;
+  }
+}
+</style>
